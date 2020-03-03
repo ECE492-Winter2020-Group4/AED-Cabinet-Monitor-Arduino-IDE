a0c0eea5e990561c6d0ca93207be92a90574f203
thebromoe <kebbi@ualberta.ca>
2020-03-03 08:23:32 -0700

    add BlueToothHandler Class

--- BLE_server_multiconnect/BLE_server_multiconnect.ino
+++ BLE_server_multiconnect/BLE_server_multiconnect.ino
@@ -10,9 +10,6 @@ bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint32_t value = 0;
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
--- include/defs.hpp
+++ include/defs.hpp
@@ -4,3 +4,6 @@
 
 #define DEEP_SLEEP_FOR_X_SECONDS 5
 #define STAY_AWAKE_FOR_X_SECONDS 5
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
