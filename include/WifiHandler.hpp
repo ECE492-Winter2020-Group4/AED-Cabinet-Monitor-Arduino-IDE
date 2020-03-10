e88efd9487e9f8c29cfadcb5c8ef6183f3a55a12
elisajdu <jiayun@ualberta.ca>
2020-03-10 16:43:09 -0600

    add attempts for wifi connection and sending email

--- include/WifiHandler.hpp
+++ include/WifiHandler.hpp
@@ -16,9 +16,9 @@ public:
     String getLocalIPAdress();
 
 private:
-    uint8_t connection_state = 0;        // Connected to WIFI or not
-    uint16_t reconnect_interval = 10000; // If not connected, wait time to try again
-    uint16_t max_reconnect_attempts = 4; // Max # of attempts for reconnecting to wifi
+    uint8_t connection_state = 0;                                  // Connected to WIFI or not
+    uint16_t reconnect_interval = 10000;                           // If not connected, wait time to try again
+    uint16_t max_reconnect_attempts = MAX_WIFI_RECONNECT_ATTEMPTS; // Max # of attempts for reconnecting to wifi
     String local_ip_address = "";
     uint8_t wifiConnect();
     void reconnect();
